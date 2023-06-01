009cf35e5ea9767f7bf1e3dc1fad7873b6a5db97
Tomás Bordaberry <tbordaberry@gmail.com>
2023-06-01 06:07:10 -0300

    Fix gitIgnore. BinaryTree schema

--- src/MyTADS/Entities/MyBinarySearchTreeImp.java
+++ src/MyTADS/Entities/MyBinarySearchTreeImp.java
@@ -1,4 +1,29 @@
 package MyTADS.Entities;
 
-public class MyBinarySearchTreeImp {
+import MyTADS.Interfaces.MyBinarySearchTree;
+
+public class MyBinarySearchTreeImp implements MyBinarySearchTree {
+
+    public MyBinarySearchTreeImp() {
+    }
+
+    @Override
+    public Object find(Comparable key) {
+        return null;
+    }
+
+    @Override
+    public void insert(Comparable key, Object data) {
+
+    }
+
+    @Override
+    public void delete(Comparable key, Object data) {
+
+    }
+
+
+
+
 }
+
