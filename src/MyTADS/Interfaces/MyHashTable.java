a39985ccd989dba57a3256d9dd6d48d1d9aecf42
ggiao2001 <135541281+ggiao2001@users.noreply.github.com>
2023-06-05 17:22:55 -0300

    Add files via upload

--- src/MyTADS/Interfaces/MyHashTable.java
+++ src/MyTADS/Interfaces/MyHashTable.java
@@ -1,9 +1,10 @@
 package MyTADS.Interfaces;
 
+import MyTADS.Exceptions.ElementNotFoundException;
+
 public interface MyHashTable<K,V> {
     void put(K key, V value);
     boolean contains(K key);
-    void remove(K key);
-    int size();
-    V get(K key);
+    void remove(K key) throws ElementNotFoundException;
+    V get(K key) throws ElementNotFoundException;
 }
