6123b1fda3f82d42c3e65b36824c3487e1b3da6d
ggiao2001 <135541281+ggiao2001@users.noreply.github.com>
2023-06-05 17:21:40 -0300

    Add files via upload

--- src/MyTADS/Entities/MyHashTableImp2.java
+++ src/MyTADS/Entities/MyHashTableImp2.java
@@ -1,10 +1,11 @@
 package MyTADS.Entities;
 
+import MyTADS.Exceptions.ElementNotFoundException;
 import MyTADS.Interfaces.MyHashTable;
 
 public class MyHashTableImp2<K,V> implements MyHashTable<K,V> {
 
-    private static final int DEFAULT_SIZE = 10;
+    private int size;
     private MyLinkedListImp<NodeHash<K,V>>[] table;
 
     //definicion del hash
@@ -13,36 +14,62 @@ public class MyHashTableImp2<K,V> implements MyHashTable<K,V> {
     }
 
     //array con linkedList
-    public MyHashTableImp2(){
-        table = new MyLinkedListImp<NodeHash<K, V>>[DEFAULT_SIZE];
+    public MyHashTableImp2(int size){
+        this.size = size;
+        table = new MyLinkedListImp<NodeHash<K, V>>[size];
         for (int i=0; i<table.length(); i++){
             table[i] = new MyLinkedListImp<>();
         }
     }
 
+    //metodos
     @Override
     public void put(K key, V value) {
         int index = hash(key);
-        MyLinkedListImp<NodeHash<K,V>>
+        MyLinkedListImp<NodeHash<K,V>> bucket = table[index]; //especifico el indice de hash que voy a usar
+        for (NodeHash<K,V> node: bucket) { //recorre todos los nodos del bucket
+            if (node.getKey() == key) { //deberia usar comparable aca??
+                node.setValue(value); //si encuentro uno con el mismo key le actualizo el valor
+                return;
+            }
+        }
+        bucket.add(new NodeHash<>(key,value));
     }
 
     @Override
     public boolean contains(K key) {
+        int index = hash(key);
+        MyLinkedListImp<NodeHash<K,V>> bucket = table[index];
+        for (NodeHash<K,V> node: bucket){
+            if (node.getKey().equals(key)){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
-    public void remove(K key) {
-
+    public void remove(K key) throws ElementNotFoundException {
+        int index = hash(key);
+        MyLinkedListImp<NodeHash<K,V>> bucket = table[index];
+        for (NodeHash<K,V> node: bucket){
+            if (node.getKey().equals(key)){
+                node = null;
+                return;
+            }
+        }
+        throw new ElementNotFoundException();
     }
 
     @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public V get(K key) {
-        return null;
+    public V get(K key) throws ElementNotFoundException {
+        int index = hash(key);
+        MyLinkedListImp<NodeHash<K,V>> bucket = table[index];
+        for (NodeHash<K,V> node: bucket){
+            if (node.getKey().equals(key)){
+                return node.getValue();
+            }
+        }
+        throw new ElementNotFoundException();
     }
 }
