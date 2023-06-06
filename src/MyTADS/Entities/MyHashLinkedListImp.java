619248356932587c7eb5dca94b2ab0d690d3f1a4
ggiao2001 <giaomatai@gmail.com>
2023-06-06 01:00:30 -0300

    HashLinkedList y HashLinkedListTest para despues usarlos en HashTableIm

---
+++ src/MyTADS/Entities/MyHashLinkedListImp.java
@@ -0,0 +1,123 @@
+package MyTADS.Entities;
+
+import MyTADS.Exceptions.InvalidInputException;
+import MyTADS.Exceptions.OutOfBoundsException;
+import MyTADS.Interfaces.MyList;
+
+public class MyHashLinkedListImp<K,V> implements MyList<NodeHash<K,V>> {
+
+    private NodeHash<K,V> first;
+    private NodeHash<K,V> last;
+
+    public MyHashLinkedListImp() {
+        this.first = null;
+        this.last = null;
+    }
+
+    public NodeHash<K, V> getFirst() {
+        return first;
+    }
+
+    public void setFirst(NodeHash<K, V> first) {
+        this.first = first;
+    }
+
+    public NodeHash<K, V> getLast() {
+        return last;
+    }
+
+    public void setLast(NodeHash<K, V> last) {
+        this.last = last;
+    }
+
+    @Override
+    public void add(NodeHash<K,V> node) {
+        if (node!=null) {
+            if (first == null && last == null) {
+                first = node;
+                last = node;
+            } else {
+                last.setNext(node);
+                last = node;
+            }
+        }
+    }
+
+    @Override
+    public NodeHash<K, V> get(int position) {
+        NodeHash<K,V> aux = first;
+        int search = 0;
+        while (search < position && aux.getNext() != null) {
+            search++;
+            aux = aux.getNext();
+        }
+        if (search == position) {
+            return aux;
+        }
+        return null;
+        //throw new OutOfBoundsException();
+    }
+
+    @Override
+    public boolean contains(NodeHash<K, V> node) {
+        if (node != null && first != null) {
+            NodeHash<K,V> aux = first;
+            while (node.getKey() != aux.getKey() && aux.getNext() != null){
+                aux = aux.getNext();
+            }
+            if (aux.getKey() == node.getKey()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void remove(NodeHash<K, V> node) throws OutOfBoundsException {
+        if (node != null && first != null) {
+            NodeHash<K,V> aux = first;
+            NodeHash<K,V> auxPrev = null;
+            while (node.getKey() != aux.getKey() && aux.getNext() != null) {
+                auxPrev = aux;
+                aux = aux.getNext();
+            }
+            if (node.getKey() == aux.getKey()) {
+                if (aux == first && aux == last) {
+                    //caso remove el unico elemento
+                    first = null;
+                    last = null;
+                } else if (aux == first) {
+                    //caso saco el first
+                    first = first.getNext();
+                    aux.setNext(null);
+                } else if (aux == last) {
+                    //caso remove el last
+                    last = auxPrev;
+                    last.setNext(null);
+                } else {
+                    //otros casos
+                    auxPrev.setNext(aux.getNext());
+                    aux.setNext(null);
+                }
+            } else {
+                //significa que recorri toda la lista pero ninguno coincide con mi key
+                throw new OutOfBoundsException();
+            }
+        }
+    }
+
+    @Override
+    public int size() {
+        if (first == null) {
+            return 0;
+        } else {
+            int size = 1;
+            NodeHash<K,V> aux = first;
+            while (aux.getNext() != null) {
+                size++;
+                aux = aux.getNext();
+            }
+        return size;
+        }
+    }
+}
